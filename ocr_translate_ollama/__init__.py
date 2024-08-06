41ec899285ea9b4ad24a0bee5f23d9042e523da1
crivella <davide.grassano@epfl.ch>
2024-08-06 16:38:30 +0200

    Bump version

--- ocr_translate_ollama/__init__.py
+++ ocr_translate_ollama/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugin to implement ollama (LLMs) based translations for ocr_translate"""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 tsl_model_data = {
     # Name of the model that will be created in the user database and displayed in the extension
